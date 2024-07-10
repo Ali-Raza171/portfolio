f9de2bba4a47874e5125333e1a9283bd6a33ef7c
Ali Raza <126044057+Ali-Raza171@users.noreply.github.com>
2024-07-10 23:47:16 +0500

    pc to cloud

---
+++ src/assets/index.js
@@ -0,0 +1,83 @@
+import parthmittal from "./parth.svg";
+import gamivision from "./gamivision.jpeg";
+import remotely from "./remotely.jpg";
+import uol from "./uol.jpeg";
+import sololearn from "./sololearn.png";
+import rollingball from "./rollingball.jpg";
+import nova_assault from "./nova_assault.jpg";
+import mini_bounce from "./mini_bounce.jpeg";
+import smash_hit from "./smash_hit.jpg";
+import ar_game from "./ar_game.jpg";
+import car_parking from "./car_parking.png";
+
+
+import profile from "./profile.jpg";
+import profilePic from "./profile-pic.jpg";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import freadom from "./freadom.jpg";
+import bank from "./bank.jpg";
+import todo from "./todo.jpg";
+import graphql from "./graphql.png";
+import portfolio from "./portfolio.jpg";
+import zulip from "./zulip.jpg";
+import publiclab from "./publiclab.png";
+import devfolio from "./devfolio.png";
+import ethforall from "./ethforall.png";
+
+export {
+  gamivision,
+  remotely,
+  uol,
+  sololearn,
+  rollingball,
+  nova_assault,
+  mini_bounce,
+  smash_hit,
+  profile,
+  ar_game,
+  car_parking,
+
+  
+  
+
+  quotes,
+  robot,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  freadom,
+  bank,
+  todo,
+  graphql,
+  portfolio,
+  publiclab,
+  zulip,
+  parthmittal,
+  profilePic,
+  devfolio,
+  ethforall
+};
---
+++ src/constants/index.js
@@ -0,0 +1,533 @@
+import {
+  gamivision,
+  remotely,
+  uol,
+  sololearn,
+  rollingball,
+  nova_assault,
+  mini_bounce,
+  smash_hit,
+  ar_game,
+  car_parking,
+
+  
+  portfolio,
+  publiclab,
+  zulip,
+  devfolio,
+  ethforall
+} from "../assets";
+
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillMail,
+  AiFillHtml5,
+} from "react-icons/ai";
+
+import {
+  
+  SiJavascript,
+  SiPython,
+  SiCplusplus,
+  SiC,
+  SiVisualstudiocode,
+  SiGit,
+  SiMysql,
+  SiFigma,
+  SiCsharp,
+  SiUnity
+} from "react-icons/si";
+
+import {
+  FaHardHat,
+  FaRust
+} from "react-icons/fa";
+
+import {
+  IoIosNotificationsOutline
+} from "react-icons/io";
+
+import {
+  FaGolang
+} from "react-icons/fa6";
+
+import { DiCss3, DiJava, DiMsqlServer, DiRuby } from "react-icons/di";
+
+export const resumeLink = "https://drive.google.com/file/d/1WEOgs_-R2oDH69DKC9BhPShlhCS-tglU/view";
+export const repoLink = "https://github.com/Ali-Raza171/portfolio";
+
+export const callToAction = "https://www.linkedin.com/in/aliraza024/"
+
+export const navLinks = [
+  {
+    id: "skills",
+    title: "Skills & Experience",
+  },
+  {
+    id: "education",
+    title: "Education",
+  },
+  {
+    id: "achievements",
+    title: "Achievements",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  
+  {
+    id: "contactMe",
+    title: "Contact Me",
+  },
+];
+
+export const educationList = [
+  {
+    id: "education-1",
+    icon: uol,
+    title: "University of Lahore",
+    degree: "Bachelor of Software Engineering",
+    duration: "Spring 2021 - Fall 2025",
+    content1: "Major: Software Engineering",
+    content2: "Minor: Data Science",
+  },
+  
+];
+
+export const achievements = [
+  {
+    id: "a-1",
+    icon: remotely,
+    event: "Remote Game Development",
+    position: "Self-learning",
+    content1: "Developed multiple games remotely, showcasing self-discipline and effective communication in virtual environments.",
+    content2: "Managed end-to-end game development cycles independently, ensuring projects were completed on schedule and to specifications.",
+    content3: "Collaborated with global teams to deliver engaging gameplay experiences, leveraging remote tools and agile methodologies.",
+    article: "",
+    project: "",
+    github: "https://github.com/Ali-Raza171",
+  },
+  {
+    id: "a-1",
+    icon: sololearn,
+    event: "Python Certification",
+    position: "Sololearn",
+    content1: "Earned a Python Intermediate certification from SoloLearn, solidifying proficiency in core programming concepts.",
+    content2: "Applied Python skills to develop game mechanics and backend systems, enhancing gameplay functionality and performance.",
+    content3: "Continuously expanded Python knowledge through practice and projects, integrating new techniques into game development workflows.",
+     
+    article: "",
+    project: "https://api2.sololearn.com/v2/certificates/CC-NVSJ1BOA/image/png",
+    github: "",
+  },
+  
+];
+
+
+export const skills = [
+  
+  {
+    title: "Skills",
+    items: [
+      {
+        id: "f-1",
+        icon: SiUnity,
+        name: "Unity 2D/3D",
+      },
+      {
+        id: "f-2",
+        icon: SiUnity,
+        name: "Level Design",
+      },
+      {
+        id: "f-3",
+        icon: SiUnity,
+        name: "Terrain Design",
+      },
+      {
+        id: "f-4",
+        icon: SiUnity,
+        name: "Game Testing",
+      },
+      {
+        id: "f-5",
+        icon: SiUnity,
+        name: "Animation",
+      },
+      {
+        id: "f-6",
+        icon: SiUnity,
+        name: "Game Mechanics",
+      },
+      {
+        id: "f-7",
+        icon: SiUnity,
+        name: "Scripting",
+      },
+      {
+        id: "f-8",
+        icon: SiUnity,
+        name: "Project Management",
+      },
+      {
+        id: "f-2",
+        icon: SiUnity,
+        name: "Problem Solving",
+      },
+    ],
+  },
+  {
+    title: "Programming Languages",
+    items: [
+      
+      {
+        id: "pl-2",
+        icon: SiPython,
+        name: "Python",
+      },
+      {
+        id: "pl-3",
+        icon: SiCplusplus,
+        name: "C++",
+      },
+    
+    
+     
+      {
+        id: "pl-7",
+        icon: SiC,
+        name: "C",
+      },
+      {
+        id: "pl-8",
+        icon: AiFillHtml5,
+        name: "HTML",
+      },
+      {
+        id: "pl-9",
+        icon: DiCss3,
+        name: "CSS",
+      },
+      {
+        id: "pl-10",
+        icon: SiJavascript,
+        name: "JavaScript",
+      },
+     
+    ],
+  },
+  {
+    title: "Others",
+    items: [
+      {
+        id: "t-1",
+        icon: SiFigma,
+        name: "Figma",
+      },
+      {
+        id: "t-2",
+        icon: SiMysql,
+        name: "MySQL",
+      },
+      
+      {
+        id: "t-4",
+        icon: SiVisualstudiocode,
+        name: "VS Code",
+      },
+      {
+        id: "t-5",
+        icon: SiGit,
+        name: "Git",
+      },
+      {
+        id: "t-6",
+        icon: AiFillGithub,
+        name: "GitHub",
+      },
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    organisation: "Gamivision",
+    logo: gamivision,
+    link: "https://www.gamivision.com/",
+    positions: [
+      {
+        title: "Associate Game Developer Intern",
+        duration: "June 2024 - Present",
+        content: [
+          {
+            text: "Excited to begin my journey at Gamivision as an Associate Game Developer, I am eager to contribute to the creation of captivating and innovative games. I look forward to learning from experienced professionals and honing my skills in game development. Together, we'll embark on building exciting projects that push the boundaries of interactive entertainment, delivering memorable gaming experiences to audiences worldwide.",
+            link: ""
+          },
+          {
+            text: "Together, we'll transform ideas into immersive gaming realities that inspire and entertain.",
+            link: ""
+          }
+        ],
+      },
+    ],
+  },
+  {
+    organisation: "Remotely",
+    logo: remotely,
+    link: "",
+    positions: [
+      {
+        title: "Self-driven game developer",
+        duration: "July 2023 - Present",
+        content: [
+          {
+            text: "I have successfully developed 4 to 5 games, some projects created entirely from scratch and others with leveraging Unity assets. This experience showcases my versatility in game development, encompassing game design, asset integration, scripting in C#, and creating compelling gameplay mechanics. Each project highlights my ability to innovate and deliver engaging gaming experiences across different genres and platforms.",
+            link: "",
+          },
+          {
+            text: "The next target is to develop AR Based shooting Game, which is my FYP as well. Skilled in remote collaboration and passionate about leveraging innovative technologies to deliver engaging gaming experiences.",
+            link: ""
+          }
+          
+        ],
+      },
+    ],
+  },
+];
+
+
+  
+
+
+export const projects = [
+  {
+    id: "project-1",
+    title: "Rolling Ball",
+    github: "https://github.com/Ali-Raza171/Rolling-Ball",
+    
+    image: rollingball,
+    content:
+      "I built a dynamic Rolling Ball game from scratch, where the ball navigates through challenging levels and dodges various obstacles. The game features a main menu for easy navigation, a scoring system to track progress, and customizable game levels to keep the gameplay fresh and engaging. This project delivers an exciting and immersive gaming experience for players.",
+       stack: [
+      {
+        id: "icon-1",
+        icon: SiCsharp,
+        name: "C#"
+      },
+     
+      
+    ],
+  },
+  {
+    id: "project-2",
+    title: "Smash Hit (Hyper casual Game)",
+    github: "https://github.com/Ali-Raza171/Smash-Hit-HyperCasual-Game",
+    
+    image: smash_hit,
+    content:
+      "Smash Hit is a captivating 2D game asset for Unity, offering dynamic gameplay where players smash blocks and navigate challenges. It features customizable levels and engaging mechanics, ideal for creating interactive gaming experiences.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiCsharp,
+        name: "C#"
+      },
+    
+    ],
+  },
+  {
+    id: "project-3",
+    title: "3D FPS ",
+    github: "https://github.com/Ali-Raza171/3D-FPS-Game",
+    
+    image: nova_assault,
+    content:
+      "Developing an immersive FPS game using Unity's free FPS asset, featuring realistic terrain and engaging shooting mechanics. Incorporating detailed environments to enhance player experience and realism.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiCsharp,
+        name: "C#"
+      },
+    ],
+  },
+  {
+    id: "project-4",
+    title: "Mini Bounce",
+    github: "https://github.com/Ali-Raza171/3D-PlateFormer",
+    
+    image: mini_bounce,
+    content:
+      "Building a dynamic 3D game from scratch where players control a jumping and moving ball, aiming to strategically hit capsules that trigger platform movements to advance through multiple challenging levels.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiCsharp,
+        name: "C#"
+      },
+    ],
+  },
+  {
+    id: "project-5",
+    title: "In Progress: AR-Based Shooting Game for Kids",
+    github: "",
+    
+    image: ar_game,
+    content:
+      "My Final Year Project (FYP), Experience an educational adventure with our AR-based shooting game designed for children. Combining the excitement of shooting gameplay with interactive math challenges, this project integrates Augmented Reality (AR) to create a fun and immersive learning experience. Children can explore AR-enhanced environments, solve math problems through gameplay, and enjoy dynamic educational entertainment.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiCsharp,
+        name: "C#"
+      },
+    ],
+  },
+  {
+    id: "project-6",
+    title: "In Progress: Car Parking",
+    github: "",
+    
+    image: car_parking,
+    content:
+      "Currently, I'm deeply engaged in developing an exciting car parking game that focuses on creating challenging levels and delivering a highly realistic gameplay experience. This project involves meticulous level design to offer players a satisfying yet demanding gameplay environment. Utilizing advanced techniques with canvas rendering, I aim to enhance realism and immersion, ensuring players enjoy a true-to-life parking simulation. Stay tuned as I strive to elevate this project to its fullest potential.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiCsharp,
+        name: "C#"
+      },
+    ],
+  },
+];
+
+export const blogPosts = [
+  {
+    id: "post-1",
+    title: "Blog Post 01 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(), // Can be edited to any string format
+    image: "https://via.placeholder.com/600/92c952",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01"
+      },
+      {
+        id: "tag-2",
+        name: "tag 03"
+      },
+      {
+        id: "tag-3",
+        name: "tag 03"
+      },
+    ],
+  },
+  {
+    id: "post-2",
+    title: "Blog Post 02 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(),
+    image: "https://via.placeholder.com/600/d32776",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01"
+      },
+      {
+        id: "tag-2",
+        name: "tag 03"
+      },
+      {
+        id: "tag-3",
+        name: "tag 03"
+      },
+    ],
+  },
+  {
+    id: "post-3",
+    title: "Blog Post 03 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(),
+    image: "https://via.placeholder.com/600/771796",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01"
+      },
+      {
+        id: "tag-2",
+        name: "tag 03"
+      },
+      {
+        id: "tag-3",
+        name: "tag 03"
+      },
+    ],
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Organisations",
+    value: "2+",
+  },
+  {
+    id: "stats-2",
+    title: "Issues Opened",
+    value: "6+",
+  },
+  {
+    id: "stats-3",
+    title: "Pull Requests",
+    value: "6+",
+  },
+];
+
+export const extraCurricular = [
+  {
+    organisation: "Devfolio",
+    title: "UniDAO Lead",
+    duration: "December 2021 - Present",
+    content: [
+      {
+        text: "Selected among 5 students across the country to lead the initiative and grow the culture of Blockchain and Ethereum, powered by Devfolio.",
+        link: "https://www.linkedin.com/feed/update/urn:li:activity:7097977924686942209/",
+      },
+      {
+        text: "Led a cohort of 37 selected students over 6 weeks to learn and build in the Ethereum ecosystem.",
+        link: "https://www.linkedin.com/feed/update/urn:li:activity:7095310520282480641/",
+      },
+    ],
+    logo: devfolio,
+  },
+  
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: AiFillLinkedin,
+    link: "https://www.linkedin.com/in/aliraza024/",
+  },
+  {
+    id: "social-media-2",
+    icon: AiFillGithub,
+    link: "https://github.com/Ali-Raza171",
+  },
+  {
+    id: "social-media-3",
+    icon: AiFillMail,
+    link: "mailto:ali.razaa6251@gmail.com",
+  },
+];
+
+export const aboutMe = {
+  name: "Ali Raza",
+  tagLine: "Game Developer | AR Enthusiast | Freelance Unity Developer | FYP AR Shooting Game",
+  intro: "Passionate game developer with experience in Unity and actively working on an AR Based Shooting Game Project, driven by a commitment to exploring innovative gaming solutions and emerging technologies."
+}
